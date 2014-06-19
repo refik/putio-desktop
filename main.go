22e0babf594886571c5c333ae92ddac8a7291cf1
Refik Turkeli <refik.rfk@gmail.com>
2014-06-19 20:53:16 +0300

    resuming downloads

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -26,7 +27,7 @@ var CheckInterval = flag.Int("check-minutes", 10, "check interval of remote file
 
 const ApiUrl = "https://api.put.io/v2/"
 const DownloadExtension = ".ptdownload"
-const MaxConnection = 10
+const MaxConnection = 5
 const ChunkSize int64 = 32 * 1024
 
 // Globals
@@ -138,6 +139,8 @@ func FilesListRequest(parentId int) (files []File, err error) {
 	return filesResponse.Files, nil
 }
 
+// Download functions
+
 func WalkAndDownload(parentId int, folderPath string) {
 	log.Println("Walking in:", folderPath)
 
@@ -169,40 +172,17 @@ func WalkAndDownload(parentId int, folderPath string) {
 	}
 }
 
-// Local file operations
-
-func FillWithZeros(fp *os.File, remainingWrite int64) error {
-	var nWrite int64 // Next chunk size to write
-	zeros := make([]byte, ChunkSize)
-	for remainingWrite > 0 {
-		// Checking whether there is less to write than chunkSize
-		if remainingWrite < ChunkSize {
-			nWrite = remainingWrite
-		} else {
-			nWrite = ChunkSize
-		}
-
-		_, err := fp.Write(zeros[0:nWrite])
-		if err != nil {
-			return err
-		}
-
-		remainingWrite -= nWrite
-	}
-	return nil
-}
-
 // Downloads the given range. In case of an error, sleeps for 10s and tries again.
-func DownloadRange(file *File, fp *os.File, offset int64, size int64, chunkWg *sync.WaitGroup) {
-	defer chunkWg.Done()
+func DownloadRange(file *File, fp *os.File, offset int64, size int64, rangeWg *sync.WaitGroup, chunkIndex bitField) {
+	defer rangeWg.Done()
 	newOffset := offset
 
 	retry := func(err error) {
 		log.Println(err, "Retrying in 10s...")
 		time.Sleep(10 * time.Second)
 		written := newOffset - offset
-		chunkWg.Add(1)
-		go DownloadRange(file, fp, newOffset, size-written, chunkWg)
+		rangeWg.Add(1)
+		go DownloadRange(file, fp, newOffset, size-written, rangeWg, chunkIndex)
 	}
 
 	// Creating a custom request because it will have Range header in it
@@ -232,7 +212,10 @@ func DownloadRange(file *File, fp *os.File, offset int64, size int64, chunkWg *s
 		if nr > 0 {
 			nw, ew := fp.WriteAt(buffer[0:nr], newOffset)
 			newOffset += int64(nw)
+			chunkIndex.Set(newOffset/ChunkSize - 1)
+			fp.WriteAt(chunkIndex, file.Size)
 			if ew != nil {
+				log.Println(newOffset)
 				retry(ew)
 				return
 			}
@@ -248,47 +231,83 @@ func DownloadRange(file *File, fp *os.File, offset int64, size int64, chunkWg *s
 }
 
 func DownloadFile(file File, path string) error {
-	log.Println("Downloading:", file.Name)
+	downloadPath := path + DownloadExtension
+	chunkIndex := bitField(make([]byte, file.Size/ChunkSize+1))
+	resume := false
+	var fp *os.File
 
 	// Creating a waitgroup to wait for all chunks to be
 	// downloaded before returning
-	var chunkWg sync.WaitGroup
+	var rangeWg sync.WaitGroup
 
-	// Creating the file under a temporary name for download
-	fp, err := os.Create(path + DownloadExtension)
-	if err != nil {
-		log.Println(err)
-		return err
+	// Checking whether previous download exists
+	if _, err := os.Stat(downloadPath); err != nil {
+		log.Println("Downloading:", file.Name)
+		fp, err = os.Create(downloadPath)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		defer fp.Close()
+
+		// Allocating space for the file
+		err = FillWithZeros(fp, file.Size+int64(len(chunkIndex)))
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+	} else {
+		log.Println("Resuming:", file.Name)
+		fp, err = os.OpenFile(downloadPath, os.O_RDWR, 0755)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+		defer fp.Close()
+		_, err = fp.ReadAt(chunkIndex, file.Size)
+		if err != nil {
+			log.Println(err)
+			return err
+		}
 	}
-	defer fp.Close()
 
-	// Allocating space for the file
-	err = FillWithZeros(fp, file.Size)
-	if err != nil {
-		log.Println(err)
-		return err
-	}
-
-	chunkSize := file.Size / MaxConnection
+	rangeSize := file.Size / MaxConnection
 	excessBytes := file.Size % MaxConnection
 
 	offset := int64(0)
-	chunkWg.Add(MaxConnection)
+	rangeWg.Add(MaxConnection)
 	for i := 0; i < MaxConnection; i++ {
 		if i == MaxConnection-1 {
 			// Add excess bytes to last connection
-			chunkSize += excessBytes
+			rangeSize += excessBytes
 		}
-		go DownloadRange(&file, fp, offset, chunkSize, &chunkWg)
-		offset += chunkSize
+		if resume {
+			// Adjusting range for previously downloaded file
+			startIndex := offset / ChunkSize
+			limitIndex := (offset + rangeSize) / ChunkSize
+
+			zIndex, err := chunkIndex.GetFirstZeroIndex(startIndex, limitIndex)
+			if err != nil {
+				zByteIndex := zIndex * ChunkSize
+				previouslyDownloaded := zByteIndex - offset
+				remaining := rangeSize - previouslyDownloaded
+				newRangeSize := remaining - (remaining % ChunkSize)
+				go DownloadRange(&file, fp, zByteIndex, newRangeSize, &rangeWg, chunkIndex)
+			}
+		} else {
+
+			go DownloadRange(&file, fp, offset, rangeSize, &rangeWg, chunkIndex)
+		}
+		offset += rangeSize
 	}
 
 	// Waiting for all chunks to be downloaded
-	chunkWg.Wait()
+	rangeWg.Wait()
 
 	// Renaming the file to correct path
+	fp.Truncate(file.Size)
 	fp.Close()
-	err = os.Rename(path+DownloadExtension, path)
+	err := os.Rename(path+DownloadExtension, path)
 	if err != nil {
 		log.Println(err)
 		return err
@@ -297,6 +316,54 @@ func DownloadFile(file File, path string) error {
 	log.Println("Download completed:", file.Name)
 	return nil
 }
+
+// Utility functions
+
+func FillWithZeros(fp *os.File, remainingWrite int64) error {
+	var nWrite int64 // Next chunk size to write
+	zeros := make([]byte, ChunkSize)
+	for remainingWrite > 0 {
+		// Checking whether there is less to write than chunkSize
+		if remainingWrite < ChunkSize {
+			nWrite = remainingWrite
+		} else {
+			nWrite = ChunkSize
+		}
+
+		_, err := fp.Write(zeros[0:nWrite])
+		if err != nil {
+			return err
+		}
+
+		remainingWrite -= nWrite
+	}
+	return nil
+}
+
+type bitField []byte
+
+// Set bit i. 0 is the most significant bit.
+func (b bitField) Set(i int64) {
+	div, mod := divMod(i, 8)
+	b[div] |= 1 << (7 - uint32(mod))
+}
+
+// Test bit i. 0 is the most significant bit.
+func (b bitField) Test(i int64) bool {
+	div, mod := divMod(i, 8)
+	return (b[div] & (1 << (7 - uint32(mod)))) > 0
+}
+
+func (b bitField) GetFirstZeroIndex(offset, limit int64) (int64, error) {
+	for pos := offset; pos < limit; pos++ {
+		if bit := b.Test(pos); bit == false {
+			return pos, nil
+		}
+	}
+	return 0, errors.New("No zero in given range")
+}
+
+func divMod(a, b int64) (int64, int64) { return a / b, a % b }
 
 func main() {
 	log.Println("Starting...")
